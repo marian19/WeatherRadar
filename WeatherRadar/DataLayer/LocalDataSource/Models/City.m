8d0b6a5e906b24dfe870d55b370ea4e775837590
Marian <marian.samy@vodafone.com>
2018-09-08 23:43:36 +0200

    add city model

---
+++ WeatherRadar/DataLayer/LocalDataSource/Models/City.m
@@ -0,0 +1,62 @@
+//
+//  City+CoreDataClass.m
+//  
+//
+//  Created by Marian on 9/8/18.
+//
+//
+
+#import "City.h"
+#import "CoreDataManager.h"
+
+@implementation City
+
++(NSString*)entityName {
+    return @"City";
+}
+
++ (BOOL)addCity:(NSString *)cityName{
+    
+    NSManagedObjectContext *managedObjectContext = CoreDataManager.sharedInstance.managedObjectContext;
+    City* fetchedCity = [City getCityWithName:cityName];
+    if (fetchedCity == nil){
+        fetchedCity = [NSEntityDescription
+                       insertNewObjectForEntityForName:[City entityName]
+                       inManagedObjectContext:managedObjectContext];
+        fetchedCity.name = cityName;
+        NSError *error = nil;
+        
+        [managedObjectContext save:&error];
+        return YES;
+    }else{
+        return NO;
+    }
+}
+
+
++(City*) getCityWithName:(NSString *)cityName{
+    
+    City* fetchedCity = nil;
+    NSManagedObjectContext *managedObjectContext = CoreDataManager.sharedInstance.managedObjectContext;
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name LIKE[cd] %@", cityName];
+    NSFetchRequest *fetchRequest = [City fetchRequest];
+    NSError *error = nil;
+    [fetchRequest setPredicate:predicate];
+    NSArray *results = [managedObjectContext executeFetchRequest:[City fetchRequest] error:&error];
+    if (results.firstObject != nil) {
+        fetchedCity = results.firstObject;
+    }
+    return fetchedCity;
+    
+}
+
+-(void)deleteCity:(City *)city{
+    NSManagedObjectContext *managedObjectContext = CoreDataManager.sharedInstance.managedObjectContext;
+    
+    [managedObjectContext deleteObject:city];
+    NSError *error = nil;
+    
+    [managedObjectContext save:&error];
+}
+
+@end
